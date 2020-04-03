e992458fccc1c4725c30d3a5eb56174cc2bbc254
skp <463467268@qq.com>
2020-04-04 02:02:01 +0800

    1

--- bin/bin.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def printBinary(self, i: int) -> str:
-        return bin(i).replace('0b', '').zfill(32)
-
-
-if __name__ == '__main__':
-    print(Solution().printBinary(10))
-    print(Solution().printBinary(3))
--- currying.py
+++
@@ -1,5 +0,0 @@
-class Currying:
-
-    def polynomialSum2(m: int, n: int) -> int:
-        return 1.
-        to(n).toList.map(pow(_, m)).sum.asInstanceOf[Long]
